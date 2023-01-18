5f6431bbb98c9073ed2611da37933744212d52ca
David Atkinson <davatk@gmail.com>
2023-01-18 18:06:35 -0500

    make minimal changes to get things running

--- docker/dev.sh
+++ docker/dev.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "Setting up SSH server"
-env | egrep -v "^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)" >> /etc/environment
-service ssh start
+#echo "Setting up SSH server"
+#env | egrep -v "^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)" >> /etc/environment
+#service ssh start
 
-echo "Launching MPS"
-nohup nvidia-cuda-mps-control -d &
+#echo "Launching MPS"
+#nohup nvidia-cuda-mps-control -d &
 
-echo "Launching rsync daemon"
-rm -f /var/run/rsyncd.pid
-nohup rsync --daemon &
+#echo "Launching rsync daemon"
+#rm -f /var/run/rsyncd.pid
+#nohup rsync --daemon &
 
-echo "Launching Jupyter"
-mkdir -p output/logs
-nohup jupyter notebook --notebook-dir . --no-browser --port=5000 --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="" >output/logs/jupyter.log 2>&1 &
+#echo "Launching Jupyter"
+#mkdir -p output/logs
+#nohup jupyter notebook --notebook-dir . --no-browser --port=5000 --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="" >output/logs/jupyter.log 2>&1 &
 
-echo "Launching docs server"
-mkdir -p docs/_build
-nohup python -m http.server --directory docs/_build 9095 >output/logs/docserver.log 2>&1 &
+#echo "Launching docs server"
+#mkdir -p docs/_build
+#nohup python -m http.server --directory docs/_build 9095 >output/logs/docserver.log 2>&1 &
 
 echo "Entering loop"
-while sleep 1000; do :; done
+#while sleep 1000; do :; done
